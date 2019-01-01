a49c42455807b2d9819668a8312b03df1263757a
Unknown <jerrywang0731@outlook.com>
2019-01-01 22:11:23 +0800

    handle int with operator

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,30 @@
-userName = input('Your Name:' )
+# simple cal 2 para with operator
+# if 判断条件1:
+#     执行语句1……
+# elif 判断条件2:
+#     执行语句2……
+# elif 判断条件3:
+#     执行语句3……
+# else:
+#     执行语句4……
 
-print('hello world : ', userName, 'newbie');
+paraOne = input('Para 1:' )
+
+paraTwo = input('Para 2:' )
+
+operator = input('Operator: ')
+
+print('exec =>>>', paraOne, ' ', operator, paraTwo)
+
+if operator == ('+'):
+	print(int(paraOne) + int(paraTwo))
+elif operator == ('-'):
+	print(int(paraOne) - int(paraTwo))
+elif operator == ('*'):
+	print(int(paraOne) * int(paraTwo))
+elif operator == ('/'):
+	print(int(paraOne) / int(paraTwo))
+else:
+	print('happy new year 2019');
+
+
